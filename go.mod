c870dbee1e33c59862c8e74a87b67e1ca7c4b308
Alexander <alexpospelov87@gmail.com>
2024-12-07 16:22:12 +0700

    Добавил поиск по заголовкам

--- go.mod
+++ go.mod
@@ -9,4 +9,7 @@ require (
 	github.com/lib/pq v1.10.9
 )
 
-require github.com/felixge/httpsnoop v1.0.3 // indirect
+require (
+	github.com/felixge/httpsnoop v1.0.3 // indirect
+	github.com/google/uuid v1.6.0
+)
--- handlers/news.go
+++ handlers/news.go
@@ -4,17 +4,78 @@ import (
 	"APIGateway/models"
 	"encoding/json"
 	"net/http"
+	"strconv"
+	"strings"
 	"sync"
 )
 
-// GetNewsList возвращает список новостей
+// GetNewsList возвращает список новостей, поддерживая фильтрацию по заголовку
 func GetNewsList(w http.ResponseWriter, r *http.Request) {
-	news := []models.NewsShortDetailed{
+	// Добавляем пагинацию
+	titleQuery := r.URL.Query().Get("title")
+	pageStr := r.URL.Query().Get("page")
+	sizeStr := r.URL.Query().Get("size")
+
+	// Конвертируем параметры пагинации в числа, используем значения по умолчанию при ошибке
+	page, err := strconv.Atoi(pageStr)
+	if err != nil || page < 1 {
+		page = 1
+	}
+	size, err := strconv.Atoi(sizeStr)
+	if err != nil || size < 1 {
+		size = 10 // указываем размер страницы по умолчанию
+	}
+
+	news := filterNewsByTitle(titleQuery)
+	start := (page - 1) * size
+	end := start + size
+
+	if start > len(news) {
+		start = len(news)
+	}
+
+	if end > len(news) {
+		end = len(news)
+	}
+
+	paginatedNews := news[start:end]
+
+	// Добавляем информацию о пагинации в ответ
+	response := struct {
+		News  []models.NewsShortDetailed `json:"news"`
+		Page  int                        `json:"page"`
+		Size  int                        `json:"size"`
+		Total int                        `json:"total"`
+	}{
+		News:  paginatedNews,
+		Page:  page,
+		Size:  size,
+		Total: len(news),
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+
+}
+
+// filterNewsByTitle фильтрует новости по заголовкам
+func filterNewsByTitle(query string) []models.NewsShortDetailed {
+	allNews := []models.NewsShortDetailed{
 		{ID: 1, Title: "Первая новость"},
 		{ID: 2, Title: "Вторая новость"},
 	}
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(news)
+	filteredNews := []models.NewsShortDetailed{}
+	for _, news := range allNews {
+		if contains(news.Title, query) {
+			filteredNews = append(filteredNews, news)
+		}
+	}
+	return filteredNews
+}
+
+// contains проверяет, содерджится ли подстрока в строке
+func contains(str, substr string) bool {
+	return strings.Contains(str, substr)
 }
 
 // FiltersNews фильтрует новости
